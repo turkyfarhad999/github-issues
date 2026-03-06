94c9a3913e835b6018aa439dc4a38255d60a8498
turkyfarhad999 <turkyfarhad99@gmail.com>
2026-03-06 22:36:09 +0600

    third

--- script/data.js
+++ script/data.js
@@ -1,7 +1,21 @@
+const countData=()=>{
+    document.getElementById("count").innerText=`${count} Issues`
+}
+const deactive=()=>{
+   const btn= document.querySelectorAll(".selbtn")
+     btn.forEach(i=>{
+        i.classList.remove("active")
+     })
+}
 const loadAlldata=()=>{
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then(res=>res.json())
     .then(i=>showData(i.data))
+}
+const loadOpenData=()=>{
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then(res=>res.json())
+    .then(i=>showOpenData(i.data))
 }
 
 // "id": 1,
@@ -17,18 +31,18 @@ const loadAlldata=()=>{
 // "assignee": "jane_smith",
 // "createdAt": "2024-01-15T10:30:00Z",
 // "updatedAt": "2024-01-15T10:30:00Z"
-
+let count=0;
 const showData=(data)=>{
 console.log(data);
 const container=document.getElementById("card-container")
 container.innerHTML=""
 for(const i of data){
-    
+    count++;
     const card=document.createElement("div")
    
     
     card.innerHTML=`
-     <div class="space-y-3 shadow-sm p-5 rounded-lg">
+     <div onclick="loadmodal(${i.id})" class="space-y-3 shadow-sm p-5 rounded-lg ">
             <div class="flex justify-between">
                 <img id="topimg" src="./assets/Open-Status.png" alt="" class="topimg">
                 <btn class="px-5 py-1 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px] ">${i.priority}</btn>
@@ -56,5 +70,46 @@ for(const i of data){
     }
     container.appendChild(card)
 }
+
+countData();
 }
+const loadmodal=(id)=>{
+    console.log(id)
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    .then(res=>res.json())
+    .then(i=>displaymodal(i.data))
+}
+const displaymodal=(data)=>{
+document.getElementById("mod").innerHTML=`
+<h1 class="font-bold text-2xl">${data.title}</h1>
+    <div class="flex justify-between items-center">
+        <button class="px-5 py-1 rounded-lg text-[#FEECEC] bg-[#00A96E] text-[12px] border-none ">opened</button>
+        <p class="text-[#64748B] text-[12px]">Opened by Turky </p>
+    </div>
+    <div class="flex flex-wrap gap2 py-3 border-b border-b-[#E4E4E7]">
+                <btn class="px-5 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px]"><i class="fa-solid fa-bug"></i>  BUG</btn>
+                <btn class="px-5 rounded-lg text-[#D97706] bg-[#FDE68A] text-[12px]"><i class="fa-solid fa-virus"></i> Help Wanter</btn>
+            </div>
+             <p class="text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices...</p>
+             <div>
+                <div>
+                    <p>Assignee:</p><br>
+                    <p>Turky</p>
+                </div>
+                <div>
+                    <p>priority</p><br>
+                    <p><btn class="px-5 py-1 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px] ">HIGH</btn></p>
+                </div>
+             </div>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-primary">Close</button>
+      </form>
+    </div>
+`
+
+document.getElementById("my_modal_1").showModal()
+}
+
 loadAlldata()
