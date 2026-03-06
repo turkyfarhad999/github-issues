3ec373f9feaa07b282e5e14e058038bce784dd40
turkyfarhad999 <turkyfarhad99@gmail.com>
2026-03-06 23:08:07 +0600

    fifth

--- script/data.js
+++ script/data.js
@@ -11,11 +11,15 @@ const loadAlldata=()=>{
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then(res=>res.json())
     .then(i=>showData(i.data))
+    deactive()
+    document.getElementById("all").classList.add("active")
 }
 const loadOpenData=()=>{
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then(res=>res.json())
     .then(i=>showOpenData(i.data))
+    deactive()
+    document.getElementById("open").classList.add("active")
 }
 
 // "id": 1,
@@ -33,6 +37,7 @@ const loadOpenData=()=>{
 // "updatedAt": "2024-01-15T10:30:00Z"
 let count=0;
 const showData=(data)=>{
+    count=0;
 console.log(data);
 const container=document.getElementById("card-container")
 container.innerHTML=""
@@ -72,7 +77,10 @@ for(const i of data){
 }
 
 countData();
+
 }
+
+loadAlldata()
 const loadmodal=(id)=>{
     console.log(id)
     fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
@@ -111,5 +119,85 @@ document.getElementById("mod").innerHTML=`
 
 document.getElementById("my_modal_1").showModal()
 }
+const showOpenData=(data)=>{
+    count=0;
+    const container=document.getElementById("card-container")
+container.innerHTML=""
+for(const i of data){
+    if(i.status !== 'open') continue;
+    count++;
+    const card=document.createElement("div")
+   
+    
+    card.innerHTML=`
+     <div onclick="loadmodal(${i.id})" class="space-y-3 shadow-sm p-5 rounded-lg ">
+            <div class="flex justify-between">
+                <img id="topimg" src="./assets/Open-Status.png" alt="" class="topimg">
+                <btn class="px-5 py-1 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px] ">${i.priority}</btn>
+            </div>
+            <h1 class="font-semibold text-[14px] ">${i.title}</h1>
+            <p class="text-[#64748B] text-[12px]">${i.description}</p>
+            <div class="flex flex-wrap gap2 py-3 border-b border-b-[#E4E4E7]">
+                <btn class="px-5 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px]"><i class="fa-solid fa-bug"></i>  BUG</btn>
+                <btn class="px-5 rounded-lg text-[#D97706] bg-[#FDE68A] text-[12px]"><i class="fa-solid fa-virus"></i> Help Wanter</btn>
+            </div>
+            <p class="text-[#64748B] text-[12px]">#${i.author}</p>
+            <p class="text-[#64748B] text-[12px]">${i.createdAt}</p>
 
-loadAlldata()
+        </div>
+    `
+     card.style.borderTop="4px solid #00A96E";
+        card.style.borderRadius="10px"
+         container.appendChild(card)
+
+    
+    }
+    
+    countData();
+    
+}
+const loadCloseData=()=>{
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then(res=>res.json())
+    .then(i=>showCloseData(i.data))
+    deactive()
+    document.getElementById("close").classList.add("active")
+}
+
+const showCloseData=(data)=>{
+    count=0;
+    const container=document.getElementById("card-container")
+container.innerHTML=""
+for(const i of data){
+    if(i.status == 'open') continue;
+    count++;
+    const card=document.createElement("div")
+   
+    
+    card.innerHTML=`
+     <div onclick="loadmodal(${i.id})" class="space-y-3 shadow-sm p-5 rounded-lg ">
+            <div class="flex justify-between">
+                <img id="topimg" src="./assets/Open-Status.png" alt="" class="topimg">
+                <btn class="px-5 py-1 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px] ">${i.priority}</btn>
+            </div>
+            <h1 class="font-semibold text-[14px] ">${i.title}</h1>
+            <p class="text-[#64748B] text-[12px]">${i.description}</p>
+            <div class="flex flex-wrap gap2 py-3 border-b border-b-[#E4E4E7]">
+                <btn class="px-5 rounded-lg bg-[#FEECEC] text-[#EF4444] text-[12px]"><i class="fa-solid fa-bug"></i>  BUG</btn>
+                <btn class="px-5 rounded-lg text-[#D97706] bg-[#FDE68A] text-[12px]"><i class="fa-solid fa-virus"></i> Help Wanter</btn>
+            </div>
+            <p class="text-[#64748B] text-[12px]">#${i.author}</p>
+            <p class="text-[#64748B] text-[12px]">${i.createdAt}</p>
+
+        </div>
+    `
+    card.style.borderTop="4px solid #A855F7";
+        card.style.borderRadius="10px"
+         container.appendChild(card)
+
+    
+    }
+    
+    countData();
+    
+}
